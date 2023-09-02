d2d3ab9e05b6de331d36ae4227882a79e8ec5232
MasterJ3y <mjahangiry75@gmail.com>
2023-09-02 15:27:23 +0330

    Remove unnecessary PngMe erros

--- src/error.rs
+++ src/error.rs
@@ -3,13 +3,10 @@ use std::fmt;
 #[derive(Debug)]
 pub enum PngMeError {
     InvalidChunkLength(String),
-    InvalidBytes(String),
     InvalidChunkType,
     InvalidCRC,
     InvalidPNGFileHeader,
     UnexistentChunkType,
-    UnableToCreateFile(String),
-    UnableToWriteOutputFile(String),
 }
 
 impl fmt::Display for PngMeError {
@@ -21,17 +18,10 @@ impl fmt::Display for PngMeError {
                 chunk,
                 chunk.len()
             ),
-            PngMeError::InvalidBytes(chunk) => write!(f, "The chunk {} has invalid bytes", chunk),
             PngMeError::InvalidChunkType => write!(f, "Invalid chunk type provided"),
             PngMeError::InvalidCRC => write!(f, "CRC IEEE Checksum didn't matched"),
             PngMeError::InvalidPNGFileHeader => write!(f, "Invalid PNG file header"),
             PngMeError::UnexistentChunkType => write!(f, "The provided chunk type doesn't exists"),
-            PngMeError::UnableToCreateFile(err_message) => {
-                write!(f, "Unable to create file: {}", err_message)
-            }
-            PngMeError::UnableToWriteOutputFile(err_message) => {
-                write!(f, "Unable to write output file: {}", err_message)
-            }
         }
     }
 }
